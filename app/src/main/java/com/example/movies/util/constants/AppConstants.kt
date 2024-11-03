91d86dd0f795e8c03d2677cb131d56b697142577
Doaa abdelkareem <doaa.abdelkareem@inovaeg.com>
2024-11-03 22:37:13 +0200

    [ENHANCE] Code - part8

--- app/src/main/java/com/example/movies/util/constants/AppConstants.kt
+++ app/src/main/java/com/example/movies/util/constants/AppConstants.kt
@@ -2,10 +2,6 @@ package com.example.movies.util.constants
 
 class AppConstants {
     companion object {
-        /*CodeReview images dimensions are too big to be used which will take a lot of time and network data to be loaded
-            check this link to see how to get a smaller size https://developers.themoviedb.org/3/getting-started/images
-            And you can use a larger size for tablets
-         */
         const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/"
         const val YOUTUBE_IMAGE_BASE_URL = "https://img.youtube.com/vi/"
         const val YOUTUBE_IMAGE_HIGH_QUALITY = "hqdefault.jpg"

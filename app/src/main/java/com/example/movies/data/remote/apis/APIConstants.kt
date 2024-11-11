1529c3856791c64199e9cc05c8323190a3976c53
Doaa abdelkareem <doaa.abdelkareem@inovaeg.com>
2024-11-11 18:05:27 +0200

    [EDIT]

--- app/src/main/java/com/example/movies/data/remote/apis/APIConstants.kt
+++ app/src/main/java/com/example/movies/data/remote/apis/APIConstants.kt
@@ -5,7 +5,7 @@ class APIConstants {
         const val BASE_URL = "https://api.themoviedb.org/3/"
         const val PARAM_API_KEY = "api_key"
         const val PARAM_LANGUAGE = "language"
-        const val API_KEY = "6675b80b92722b89ee281264add1a4e6"
+        const val API_KEY = ""
         const val LANGUAGE  = "en-US"
         const val PAGE = 1
         const val TAG_OK_HTTP = "OkHttp"
